dc32aa4b07f740091d89668e8c443924722e1c69
Ram Dettmer <ramdettmer751@gmail.com>
2022-06-08 19:07:06 -0700

    update

--- src/pages/legal/index.js
+++ src/pages/legal/index.js
@@ -1,19 +1,47 @@
+import { graphql, Link } from "gatsby"
 import * as React from "react"
 import Layout from "../../components/global/Layout"
 import Seo from "../../components/global/Seo"
+import Intro from "../../components/blocks/Intro"
 
-export default function LegalList() {
+export default function LegalList({ data }) {
+    
+    const frontMatter = data.allLegal
+
     return (
         <Layout>
             <Seo
                 title="Legal"
-                description=""
+                description="View our website policies"
             />
-            <div className="section">
-                <div className="container">
-
+            <Intro 
+                heading="Legal Policies"
+                body="View our website policies"
+            />
+            <div className="section-large">
+                <div className="container text-center">
+                    <ul>
+                    {frontMatter.nodes.map((node) => {
+                        return (
+                            <li className="text-lg"><Link to={"/legal" + node.slug}>{node.frontmatter.title}</Link></li>
+                        )
+                    })}
+                    </ul>
                 </div>
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+{
+    allLegal {
+      nodes {
+        frontmatter {
+          title
+        }
+        slug
+      }
+    }
+  }
+`
